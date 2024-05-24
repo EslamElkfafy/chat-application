e97cf6987d60768f067be26443dc4b77647e2ab5
elkfafy <hmdeslam.adel@gmail.com>
2024-05-24 15:22:52 +0300

    simple

--- chat-application-main/src/components/_elements/UploadButton.tsx
+++ chat-application-main/src/components/_elements/UploadButton.tsx
@@ -46,7 +46,7 @@ function UploadButton() {
       className="bg-gray-50  border border-gray-700 rounded-md text-sm  py-1.5 outline-none w-full   cursor-pointer mx-auto block font-[sans-serif]"
     >
       <div className="px-1 flex items-center  w-full" >
-      <img src={user.img} className="w-5 h-5"/>
+      <img src={import.meta.env.VITE_API_BASE_URL + user.img} className="w-5 h-5"/>
       <p className="text-center w-full font-bold text-gray-500">        تغير الصوره</p>
       </div>
       <input type="file" id="uploadFile1" className="hidden" accept="image/png" onChange={handleFileInputChange}/>
