da53ed6d82f47630bad6bad3b12d2f986be652cc
elkfafy <hmdeslam.adel@gmail.com>
2024-05-24 15:14:42 +0300

    simple

--- chat-application-main/src/components/UserOnlineContainer.tsx
+++ chat-application-main/src/components/UserOnlineContainer.tsx
@@ -12,7 +12,7 @@ function UserOnlineContainer({ onClick, user }: { onClick?: () => void, user: an
       }}
     >
       <div className="flex items-center gap-x-1">
-        <img src={user.img} className="w-9 h-9" style={{borderLeft: user.status === "connect" ? ((user.chatBlock === true || user.infoBlock === true) ? "4px solid red" : "4px solid green") :  "4px solid yellow"}}/>
+        <img src={import.meta.env.VITE_API_BASE_URL + user.img} className="w-9 h-9" style={{borderLeft: user.status === "connect" ? ((user.chatBlock === true || user.infoBlock === true) ? "4px solid red" : "4px solid green") :  "4px solid yellow"}}/>
         <div className="flex flex-col justify-between py-2">
           <p style={{color: user.nameColor, backgroundColor: user.backgroundColor}}>{user.name}</p>
           <p style={{color: user.fontColor}}>{user.state}</p>
