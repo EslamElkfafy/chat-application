fc83f4771f878a250f516c208441621678f17b4a
elkfafy <hmdeslam.adel@gmail.com>
2024-05-24 15:07:42 +0300

    simple

--- chat-application-main/src/components/Message.tsx
+++ chat-application-main/src/components/Message.tsx
@@ -5,7 +5,7 @@ function Message({item} : {item : any}) {
   return (
     <div  className="flex justify-between items-center w-full px-1" style={{borderBottom: "1px solid #bbaeae"}}>
       <div className="flex items-center gap-x-2">
-        <img src={item.img} className="w-11 h-11"/>
+        <img src={import.meta.env.VITE_API_BASE_URL + item.img} className="w-11 h-11"/>
         <div className="flex flex-col  py-2">
             <p className={"font-bold"} style={{color: item.nameColor, backgroundColor: item.backgroundColor, textAlign:"center", width: "fit-content", padding: "0px 10px"}}>{item.name}</p>
             <p style={{color: item.fontColor}}>{item.description}</p>
